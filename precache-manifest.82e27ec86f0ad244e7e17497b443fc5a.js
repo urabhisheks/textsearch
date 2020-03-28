9aaf83f2668123d06b076308ea7400abc564b294
Abhishek Kumar <urabhisheks@gmail.com>
2020-03-28 13:37:14 +0530

    Updates

---
+++ precache-manifest.82e27ec86f0ad244e7e17497b443fc5a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b8b2950c065ef4f2a14490b616cc3d",
+    "url": "/textsearch/index.html"
+  },
+  {
+    "revision": "9d33af405f57552c61c4",
+    "url": "/textsearch/static/css/2.527b9448.chunk.css"
+  },
+  {
+    "revision": "7be288a1c438c5b972b1",
+    "url": "/textsearch/static/css/main.adb19d58.chunk.css"
+  },
+  {
+    "revision": "9d33af405f57552c61c4",
+    "url": "/textsearch/static/js/2.9ae929a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/textsearch/static/js/2.9ae929a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be288a1c438c5b972b1",
+    "url": "/textsearch/static/js/main.12d70698.chunk.js"
+  },
+  {
+    "revision": "4f144ba629adea6c1153",
+    "url": "/textsearch/static/js/runtime-main.51ee8cd2.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/textsearch/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/textsearch/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/textsearch/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/textsearch/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/textsearch/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
